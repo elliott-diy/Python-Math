98885d76cdf93a1fcb49db17290f948dbd116dcc
Duplexes <63378937+Duplexes@users.noreply.github.com>
2021-07-31 16:30:26 -0700

    Added better foramting and prime.py

--- vibratingmass.py
+++ vibratingmass.py
@@ -2,48 +2,62 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Program Settings
+
 n = 10
-
-#Function that takes in X returns Cos(X)
-def sinXiteration(i,x):
-    return (((-1)**i) * (x**((2*i)+1))) / math.factorial((2*i)+1)
-
-def sinX(n,x):
-    answer = 0
-    for i in range (0,n):
-        answer += sinXiteration(i,x)
-    
-    return answer
-
-#Function that takes in X retrunes Cos(x
-def cosXiteration(i,x):
-    return (((-1)**i)*(x**(2*i)))/math.factorial(2*i)
-
-def cosX(n,x):
-    answer = 0
-    for i in range (0,n):
-        answer += cosXiteration(i,x)
-    
-    return answer
-
-
-x0 = -4 # Start displacement
-xDot0 = 5 #Start velicoty 
+x0 = -4  # Start displacement
+xDot0 = 5  # Start velicoty
 k = 40
 m = 10
 c = 5
-wn = math.sqrt(k/m)
+wn = math.sqrt(k / m)
+
+
+# Function that takes in X returns Cos(X)
+
+def sinXiteration(i, x):
+    return (-1) ** i * x ** (2 * i + 1) / math.factorial(2 * i + 1)
+
+
+def sinX(n, x):
+    answer = 0
+    for i in range(0, n):
+        answer += sinXiteration(i, x)
+
+    return answer
+
+
+# Function that takes in X retrunes Cos(x
+
+def cosXiteration(i, x):
+    return (-1) ** i * x ** (2 * i) / math.factorial(2 * i)
+
+
+def cosX(n, x):
+    answer = 0
+    for i in range(0, n):
+        answer += cosXiteration(i, x)
+
+    return answer
+
+
+# Position and velocity calculation
+
 def position(t):
-    return (x0  * math.cos(wn * t) + (xDot0/wn) * math.sin(wn * t))
+    return x0 * math.cos(wn * t) + xDot0 / wn * math.sin(wn * t)
+
 
 def velocity(t):
-    return (-x0*wn*sinX(n,wn*t) + ((xDot0)*cosX(n,wn*t)))
+    return -x0 * wn * math.sin(wn * t) + xDot0 * math.cos(wn * t)
+
+
+# Position and velocity grapher
 
 def positionGrapher():
     xValues = []
     yValues = []
-    yValuesVelocity = [] 
-    for j in np.linspace(0,20,100):
+    yValuesVelocity = []
+    for j in np.linspace(0, 20, 100):
         xValues.append(j)
         yValues.append(position(j))
         yValuesVelocity.append(velocity(j))
@@ -52,5 +66,6 @@ def positionGrapher():
     plt.ylabel('Position')
     plt.xlabel('Time')
     plt.show()
+
 
 positionGrapher()
